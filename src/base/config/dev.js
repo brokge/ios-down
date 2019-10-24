4ee4996608fefff62997694f4d8b217983dfc2b4
dabing <dabing@2dfire.com>
2019-10-24 10:38:51 +0800

    feat: 更改证书，修改http为https

--- src/base/config/dev.js
+++ src/base/config/dev.js
@@ -4,6 +4,6 @@ module.exports = {
     //API_BASE_URL: 'http://10.1.25.194:8080/integral-api/'
     //发布项目环境
     //API_BASE_URL: 'http://10.1.69.31:9200/data/xiaozhushou/'
-    API_BASE_URL: 'http://10.1.65.248:5000/' 
+    API_BASE_URL: 'https://10.1.65.248:5000/' 
 
 }
